858795da4a7b0742b52df6d697f484808d94e260
Narendra Pathai <narendra.pathai@gmail.com>
2021-05-09 09:23:08 -0400

    Initialized Tic Tac Toe game with empty board:

---
+++ src/main/java/org/npathai/tictactoe/TicTacToe.java
@@ -0,0 +1,10 @@
+package org.npathai.tictactoe;
+
+public class TicTacToe {
+
+    public void start() {
+        System.out.println("_|_|_");
+        System.out.println("_|_|_");
+        System.out.println("_|_|_");
+    }
+}
